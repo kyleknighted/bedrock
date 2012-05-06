d9e9f6d58563092be17d04152c21899785a7998b
dylan hassinger <d@dylanized.com>
2012-05-06 13:17:09 -0500

    deleting all files, starting over

--- wp-content/themes/bedrock/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	
-	if (is_home() || is_category() || is_archive || is_single()) {
-			dynamic_sidebar('Main Sidebar');
-		} else {
-			dynamic_sidebar('Blog Sidebar');	
-	}
-?>
-<?php // display_lipsum(3); ?>
