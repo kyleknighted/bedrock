9274b24c43d9cbcde4566e029a99f85b7db05828
dylan hassinger <d@dylanized.com>
2012-05-07 09:43:45 -0500

    added layout-b template

---
+++ layout_b.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Layout B Template (Left Sidebar)
+*/
+?>
+<?php get_header(); ?>
+	
+	<div class="column-wrapper layout-b">					
+		
+		<div id="main-col" class="column">		
+		<div class="inner common">
+
+			<?php get_template_part('_loop'); ?>
+			
+		</div>		
+		</div>				
+	
+		<div id="left-col" class="column sidebar">
+		<div class="inner common common-form">
+		
+			<?php get_sidebar(); ?>
+		
+		</div>
+		</div>				
+					
+	</div>
+
+<?php get_footer(); ?>
